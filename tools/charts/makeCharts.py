fa29efd8c2adb1f4dd7fe52a02b8859fe3c65f85
4-en <benschumacher0@gmail.com>
2023-01-25 17:00:44 +0100

    charts from tests

--- tools/charts/makeCharts.py
+++ tools/charts/makeCharts.py
@@ -21,18 +21,74 @@ def loadTest(folder: str) -> list:
     return data
 
 def getTotalTimes(data: list) -> list:
-    return [30 + random.randint(0, 30) for i in range(10)]
-    return [d["totalTime"] for d in data]
+    #return [30 + random.randint(0, 30) for i in range(10)]
+    times = []
+    for d in data:
+        times.append(float(d["totalDurationSeconds"]))
+    return times
 
 def getUsedFeatures(data: list) -> list:
     features = ["Type", "Categories", "Subcategories", "Search", "Sort"]
-    counts = [ random.randint(0, 10) for i in range(len(features)) ]
+    #counts = [ random.randint(0, 10) for i in range(len(features)) ]
+    totalTasks = 0
+    counts = [0, 0, 0, 0, 0]
+    for d in data:
+        for t in d["tasks"]:
+            totalTasks += 1
+            usedType = False
+            usedCategories = False
+            usedSubcategories = False
+            usedSearch = False
+            usedSort = False
+            for e in t["entries"]:
+                if e["name"] == "typeButton":
+                    usedType = True
+                elif e["name"] == "categoryButton":
+                    usedCategories = True
+                elif e["name"] == "subcategoryButton":
+                    usedSubcategories = True
+                elif e["name"] == "searchFieldInput":
+                    usedSearch = True
+                elif e["name"] == "sortButton":
+                    usedSort = True
+            if usedType:
+                counts[0] += 1
+            if usedCategories:
+                counts[1] += 1
+            if usedSubcategories:
+                counts[2] += 1
+            if usedSearch:
+                counts[3] += 1
+            if usedSort:
+                counts[4] += 1
+
+    for i in range(len(counts)):
+        counts[i] /= totalTasks
+        counts[i] *= 100
+    
     return features, counts
 
 def getAvgTestTime(data: list) -> list:
-    testNames = ["Test 1", "Test 2", "Test 3", "Test 4", "Test 5", "Test 6", "Test 7", "Test 8", "Test 9", "Test 10"]
-    times = [ random.randint(0, 10) for i in range(len(testNames)) ]
+    testNames = []
+    times = []
+
+    for i, t in enumerate(data[0]["tasks"]):
+        testNames.append("Test " + str(i + 1))
+        times.append(0)
+
+    for d in data:
+        for i, t in enumerate(d["tasks"]):
+            times[i] += float(t["durationSeconds"])
+    for i in range(len(times)):
+        times[i] /= len(data)
+
     return testNames, times
+
+def getOS(data: list) -> list:
+    os = []
+    for d in data:
+        os.append(d["os"])
+    return os
 
 def main():
     # get folder with test data from user
@@ -52,7 +108,7 @@ def main():
     plt.xlabel("Test")
     plt.ylabel("Time (s)")
     times = getTotalTimes(data)
-    plt.bar(range(len(times)), times)
+    plt.bar(["User " + str(i+1) for i in range(len(times))], times)
     plt.savefig("totalTime.png", dpi=300)
 
     # total time (histogram)
@@ -64,12 +120,15 @@ def main():
     plt.savefig("totalTimeHistogram.png", dpi=300)
 
 
-    # used features
+    # used features in percent
     plt.clf()
     plt.title("Used Features")
     plt.xlabel("Feature")
-    plt.ylabel("Count")
+    plt.ylabel("% of Tasks")
     features, counts = getUsedFeatures(data)
+    # y axis is in percent
+    plt.ylim(0, 100)
+
     plt.bar(features, counts)
     plt.savefig("usedFeatures.png", dpi=300)
 
@@ -87,6 +146,22 @@ def main():
     testNames, times = getAvgTestTime(data)
     plt.bar(testNames, times)
     plt.savefig("avgTestTime.png", dpi=300)
+
+    # get os data
+    os = getOS(data)
+    osnames = []
+    oscounts = []
+    for o in os:
+        if o not in osnames:
+            osnames.append(o)
+            oscounts.append(1)
+        else:
+            oscounts[osnames.index(o)] += 1
+    plt.clf()
+    plt.title("OS")
+    plt.pie(oscounts, labels=osnames)
+    plt.savefig("os.png", dpi=300)
+
 
 
 
