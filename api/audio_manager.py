f6f52044e1a0bf201fb8620e620227812745341a
4-en <ben.schumacher0@gmail.com>
2023-05-24 23:00:55 +0200

    added more functions to AudioManager

--- api/audio_manager.py
+++ api/audio_manager.py
@@ -1,50 +1,51 @@
 # contains backend logic for audio manager
 import hashlib
+from audiotypes import User, AudioContent, Author, UserContent
 
-class IAudioManager:
+class AbstractAudioManager:
 
     def __init__(self):
         self.storeState = 0
         self.userStates = {}
 
     # database functions to be implemented by child classes
-    def _create_user(self, username: str, passhash: str, salt: str, email: str, balance:int, admin: bool) -> dict:
+    def _create_user(self, user:User) -> User:
         """
         Create user
         """
         raise NotImplementedError
     
-    def edit_user(self, user_id: int, username: str, passhash: str, salt: str, email: str, balance:int, admin: bool) -> bool:
+    def _edit_user(self, user:User) -> bool:
         """
         Edit user
         """
         raise NotImplementedError
     
-    def delete_user(self, user_id: int) -> bool:
+    def _delete_user(self, user_id: int) -> bool:
         """
         Delete user
         """
         raise NotImplementedError
     
-    def get_user_by_name(self, username: str) -> dict:
+    def _get_user_by_name(self, username: str) -> User:
         """
         Get user by username
         """
         raise NotImplementedError
     
-    def get_user_by_id(self, user_id: int) -> dict:
+    def _get_user_by_id(self, user_id: int) -> User:
         """
         Get user by user id
         """
         raise NotImplementedError
     
-    def get_user_by_session_id(self, session_id: str) -> dict:
+    def _get_user_by_session_id(self, session_id: str) -> User:
         """
         Get user by session id
         """
         raise NotImplementedError
     
-    def create_user_item(self, user_id: int, item_id: int) -> dict:
+    def _create_user_item(self, user_content:UserContent) -> UserContent:
         """
         Create user item
         """
@@ -62,57 +63,63 @@ class IAudioManager:
         """
         raise NotImplementedError
     
-    def get_store_item_by_id(self, item_id: int) -> dict:
+    def get_store_item_by_id(self, item_id: int) -> AudioContent:
         """
         Get store item by item id
         """
         raise NotImplementedError
     
-    def create_store_item(self, name: str, price: int, description: str) -> dict:
+    def _create_store_item(self, audioContent:AudioContent) -> AudioContent:
         """
         Create store item
         """
         raise NotImplementedError
     
-    def edit_store_item(self, item_id: int, name: str, price: int, description: str) -> bool:
+    def _edit_store_item(self, audioContent:AudioContent) -> bool:
         """
         Edit store item
         """
         raise NotImplementedError
     
-    def delete_store_item(self, item_id: int) -> bool:
+    def _delete_store_item(self, item_id: int) -> bool:
         """
         Delete store item
         """
         raise NotImplementedError
     
-    def create_author(self, first_name: str, last_name: str) -> dict:
+    def _create_author(self, author:Author) -> Author:
         """
         Create author
         """
         raise NotImplementedError
     
-    def edit_author(self, author_id: int, first_name: str, last_name: str) -> bool:
+    def _edit_author(self, author:Author) -> bool:
         """
         Edit author
         """
         raise NotImplementedError
     
-    def delete_author(self, author_id: int) -> bool:
+    def _delete_author(self, author_id: int) -> bool:
         """
         Delete author
         """
         raise NotImplementedError
     
-    def get_author_by_id(self, author_id: int) -> dict:
+    def get_author_by_id(self, author_id: int) -> Author:
         """
         Get author by author id
         """
         raise NotImplementedError
     
-    def get_author_by_name(self, first_name: str, last_name: str) -> dict:
+    def get_author_by_name(self, first_name: str, last_name: str) -> Author:
         """
         Get author by name
+        """
+        raise NotImplementedError
+    
+    def _update_session_id(self, user_id: int, session_id: str, session_timeout:int) -> bool:
+        """
+        Update session id
         """
         raise NotImplementedError
     
@@ -120,35 +127,251 @@ class IAudioManager:
     
 
     
-    # audio manager functions that can use database functions
-    def _store_change(self):
+    # audio manager functions that are built on top of database functions
+    def notify_store_change(self):
         """
-        Called when store changes
+        Called when store state changes
         Used to update store store in ui
         """
         self.storeState += 1
 
-    def _user_change(self, username: str):
+    def notify_user_change(self, user_id: int):
         """
-        Called when user changes
+        Called when user state changes
         Used to update user lib in ui
         """
-        if username not in self.userStates:
-            self.userStates[username] = 0
-        self.userStates[username] += 1
+        if user_id not in self.userStates:
+            self.userStates[user_id] = 0
+        self.userStates[user_id] += 1
 
     def get_store_state(self) -> int:
         """
         Get store state"""
         return self.storeState
     
-    def get_user_state(self, username: str) -> int:
+    def get_user_state(self, user_id: int) -> int:
         """
         Get user state
         """
-        if username not in self.userStates:
-            self.userStates[username] = 0
-        return self.userStates[username]
+        if user_id not in self.userStates:
+            self.userStates[user_id] = 0
+        return self.userStates[user_id]
+    
+    def login(self, username: str, password: str) -> User:
+        """
+        Login
+        Returns user with session id if successful
+        """
+        user = self._get_user_by_name(username)
+        if user is None:
+            return None
+        
+        if not user.check_password(password):
+            return None
+        
+        if not user.check_session(user.session_id):
+            # generate new session id
+            user.create_new_session()
+            self._update_session_id(user.user_id, user.session_id, user.session_timeout)
+        else:
+            # extend session timeout
+            user.extend_session()
+            self._update_session_id(user.user_id, user.session_id, user.session_timeout)
+
+        return user
+    
+    def logout(self, session_id) -> bool:
+        """
+        Logout
+        """
+        user = self._get_user_by_session_id(session_id)
+        if user is None:
+            return False
+        
+        user.session_id = "no_session"
+        user.session_timeout = 0
+        self.update_session_id(user.user_id, user.session_id, user.session_timeout)
+        self.notify_user_change(user.user_id)
+        return True
+    
+    def register(self, username: str, password: str, email:str):
+        """
+        Register
+        """
+        user = User.create_new(username, password, email, balance=20000)
+        return self._create_user(user)
+    
+    def _get_user_library_by_id(self, user_id: int) -> list:
+        """
+        Get user library by user id
+        """
+        user_content = self._get_user_library(user_id)
+        if user_content is None:
+            return None
+        store_content = self.get_store_library()
+        if store_content is None:
+            return None
+        user_library = []
+        store_i = 0
+        for item in user_content:
+            # find corresponding store item
+            while store_i < len(store_content) and store_content[store_i].item_id != item.item_id:
+                store_i += 1
+            if store_i < len(store_content):
+                # create user library item
+                audio_user_content = {
+                    "rating": item.rating,
+                    "progress": item.progress,
+                    "purchased": item.purchased,
+                    **store_content[store_i].to_dict()
+                }
+                user_library.append(audio_user_content)
+        return user_library
+    
+    def get_user_library_by_session_id(self, session_id: str) -> list:
+        """
+        Get user library by session id
+        """
+        user = self._get_user_by_session_id(session_id)
+        if user is None or user.check_session(session_id) is False:
+            return None
+        return self._get_user_library_by_id(user.user_id)
+    
+    def is_admin(self, session_id: str) -> bool:
+        """
+        Check if user is admin
+        """
+        user = self._get_user_by_session_id(session_id)
+        if user is None or user.check_session(session_id) is False:
+            return False
+        return user.admin
+    
+    def get_user_library_by_id(self, target_id:int, my_session_id: str) -> list:
+        """
+        Get user library by user id
+        """
+        # check if user is admin
+        if not self.is_admin(my_session_id):
+            return None
+        
+        return self._get_user_library_by_id(target_id)
+    
+    def buy_item(self, session_id: str, item_id: int) -> bool:
+        """
+        Buys item and adds it to user library
+        Removes price from user balance
+        Returns true if successful
+        """
+        user = self._get_user_by_session_id(session_id)
+        if user is None or user.check_session(session_id) is False:
+            return False
+        store_item = self.get_store_item_by_id(item_id)
+        if store_item is None:
+            return False
+        if store_item.price > user.balance:
+            return False
+        user.balance -= store_item.price
+        self.update_user(user)
+        user_content = UserContent.create_new(user.user_id, store_item.item_id)
+        self._create_user_item(user_content)
+        self.notify_user_change(user.user_id)
+        return True
+    
+    def _get_user_item(self, user_id: int, item_id: int) -> UserContent:
+        """
+        Get user item by user id and item id
+        """
+        user_library = self._get_user_library(user_id)
+        if user_library is None:
+            return None
+        for item in user_library:
+            if item.item_id == item_id:
+                return item
+        return None
+    
+    def rate_item(self, session_id: str, item_id: int, rating: int) -> bool:
+        """
+        Rate item
+        Returns true if successful
+        """
+        user = self._get_user_by_session_id(session_id)
+        if user is None or user.check_session(session_id) is False:
+            return False
+        user_content = self._get_user_item(user.user_id, item_id)
+        if user_content is None:
+            return False
+        user_content.rating = rating
+        self._update_user_item(user_content)
+        self.notify_user_change(user.user_id)
+        return True
+    
+    def update_progress(self, session_id: str, item_id: int, progress: int) -> bool:
+        """
+        Update progress
+        Returns true if successful
+        """
+        user = self._get_user_by_session_id(session_id)
+        if user is None or user.check_session(session_id) is False:
+            return False
+        user_content = self._get_user_item(user.user_id, item_id)
+        if user_content is None:
+            return False
+        user_content.progress = progress
+        self.update_user_item(user_content)
+        self.notify_user_change(user.user_id)
+        return True
+    
+
+    ### admin functions
+    ### always check if user is admin before calling these functions
+    def create_store_item(self, session_id: str, item: AudioContent) -> bool:
+        """
+        Create store item
+        Returns true if successful
+        """
+        # check if user is admin
+        if not self.is_admin(session_id):
+            return False
+        
+        ret =  self._create_store_item(item)
+        if ret is False:
+            return False
+        self.notify_store_change()
+        return True
+    
+    def update_store_item(self, session_id: str, item: AudioContent) -> bool:
+        """
+        Update store item
+        Returns true if successful
+        """
+        # check if user is admin
+        if not self.is_admin(session_id):
+            return False
+        
+        ret = self._edit_store_item(item)
+        if ret is False:
+            return False
+        self.notify_store_change()
+        return True
+    
+    def delete_store_item(self, session_id: str, item_id: int) -> bool:
+        """
+        Delete store item
+        Returns true if successful
+        """
+        # check if user is admin
+        if not self.is_admin(session_id):
+            return False
+        
+        ret = self._delete_store_item(item_id)
+        if ret is False:
+            return False
+        self.notify_store_change()
+        return True
+    
+
+
+
     
     
     
---
+++ api/audiotypes.py
@@ -0,0 +1,136 @@
+# classes for data used in audio ui
+import time
+import hashlib
+import random
+import uuid
+
+# AudioType
+class AudioType:
+    Audiobook = 0
+    Podcast = 1
+
+
+# User
+class User:
+    def __init__(self, user_id:int, username:str, passhash:str, salt:str, email:str, session_id:str, session_timeout:int, created:int, balance:int, admin:bool):
+        self.user_id = user_id
+        self.username = username
+        self.passhash = passhash
+        self.salt = salt
+        self.email = email
+        self.session_id = session_id
+        self.session_timeout = session_timeout
+        self.created = created
+        self.balance = balance
+        self.admin = admin
+
+    @staticmethod
+    def get_password_hash(password:str, salt:str):
+        return hashlib.sha512((password + salt).encode()).hexdigest()
+
+    @classmethod
+    def create_new(cls, username:str, password:str, email:str, balance:int=10000):
+        salt = uuid.uuid4().hex
+        passhash = cls.get_password_hash(password, salt)
+        return cls(-1, username, passhash, salt, email, "no_session", 0, int(time.time()), balance, False)
+    
+    def check_password(self, password:str):
+        return self.passhash == self.get_password_hash(password, self.salt)
+    
+    def create_new_session(self):
+        self.session_id = uuid.uuid4().hex
+        self.session_timeout = int(time.time()) + 60 * 60 * 24 * 7 # 7 days
+        return self.session_id
+    
+    def check_session(self, session_id:str):
+        return self.session_id == session_id and self.session_timeout > int(time.time())
+    
+    def extend_session(self):
+        self.session_timeout = int(time.time()) + 60 * 60 * 24 * 7
+
+    def to_dict(self):
+        return {
+            "user_id": self.user_id,
+            "username": self.username,
+            "email": self.email,
+            "balance": self.balance,
+            "admin": self.admin,
+            "created": self.created,
+            "session_id": self.session_id
+        }
+    
+class AudioContent:
+    def __init__(self, content_id:int, audio_type:int, title:str, author_id:int, categories:str, series:str, duration:int, rating:float, price:int, cover_file:str, releaseDate:int, addedDate:int, series_index:int):
+        self.content_id = content_id
+        self.title = title
+        self.author_id = author_id
+        self.categories = categories
+        self.series = series
+        self.duration = duration
+        self.rating = rating
+        self.price = price
+        self.cover_file = cover_file
+        self.releaseDate = releaseDate
+        self.addedDate = addedDate
+        self.series_index = series_index
+        self.audio_type = audio_type
+    
+    def to_dict(self):
+        return {
+            "content_id": self.content_id,
+            "title": self.title,
+            "author_id": self.author_id,
+            "categories": self.categories,
+            "series": self.series,
+            "duration": self.duration,
+            "rating": self.rating,
+            "price": self.price,
+            "cover_file": self.cover_file,
+            "releaseDate": self.releaseDate,
+            "addedDate": self.addedDate,
+            "series_index": self.series_index,
+            "audio_type": self.audio_type
+        }
+    
+class Author:
+    def __init__(self, author_id:int, first_name:str, last_name:str, bio:str):
+        self.author_id = author_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.bio = bio
+
+    def to_dict(self):
+        return {
+            "author_id": self.author_id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "bio": self.bio
+        }
+    
+class UserContent:
+    def __init__(self, user_content_id, user_id:int, content_id:int, rating:float, progress:int, purchased:int, last_played:int):
+        self.user_content_id = user_content_id
+        self.user_id = user_id
+        self.content_id = content_id
+        self.rating = rating
+        self.progress = progress
+        self.purchased = purchased
+        self.last_played = last_played
+
+    @classmethod
+    def create_new(cls, user_id:int, content_id:int):
+        return cls(-1, user_id, content_id, -1, 0, int(time.time()), -1)
+    
+    def to_dict(self):
+        return {
+            "user_id": self.user_id,
+            "content_id": self.content_id,
+            "rating": self.rating,
+            "progress": self.progress,
+            "purchased": self.purchased
+        }
+    
+
+    
+
+
