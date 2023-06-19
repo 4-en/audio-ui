dbfa32e17beff05c8ba8bfb870eb726efeddeba3
4-en <benschumacher0@gmail.com>
2023-06-19 23:38:24 +0200

    removed missing image reference

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
-import AudioBild from './images/AudioBild.jpg'
+//import AudioBild from './images/AudioBild.jpg'
 
 
 class Home extends React.Component {
